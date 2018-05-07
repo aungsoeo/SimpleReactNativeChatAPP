1258963fa4714b91885c6c08b46862df673fc2f8
aungsoeoo.94s@gmail.com <aungsoeoo.94s@gmail.com>
2018-05-07 14:15:31 +0630

    more beautiful ui

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
   View,
-  ScrollView,
-  TextInput,
-  TouchableOpacity
 } from 'react-native';
 
-import { GiftedChat } from 'react-native-gifted-chat';
-
+import {GiftedChat, Actions, Bubble, SystemMessage} from 'react-native-gifted-chat';
+import CustomActions from './CustomActions';
+import CustomView from './CustomView';
 import SocketIOClient from 'socket.io-client';
 var $this;
-export default class App extends Component {
+export default class Example extends React.Component {
   constructor(props) {
     super(props);
     this.socket = SocketIOClient('http://192.168.211.174:3000');
@@ -28,59 +20,197 @@ export default class App extends Component {
       $this.onReceive(data);
     });
     this.state = {
-      messages: []
-    }
+      messages: [],
+      loadEarlier: true,
+      typingText: null,
+      isLoadingEarlier: false,
+    };
+
+    this._isMounted = false;
+    this.onSend = this.onSend.bind(this);
+    this.renderCustomActions = this.renderCustomActions.bind(this);
+    this.renderBubble = this.renderBubble.bind(this);
+    this.renderSystemMessage = this.renderSystemMessage.bind(this);
+    this.renderFooter = this.renderFooter.bind(this);
+    this.onLoadEarlier = this.onLoadEarlier.bind(this);
+
+    this._isAlright = null;
   }
-  componentWillMount() {
+
+  componentWillMount() {   //need to fetch real data with api
+    this._isMounted = true;
     this.setState({
       messages: [
         {
-          _id: 1,
-          text: 'Hello developer',
+          _id: Math.round(Math.random() * 1000000),
+          text: 'Are you building a chat app?',
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: 'React Native',
-            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+            name: 'Admin',
           },
         },
       ],
     })
   }
-  onReceive(text) {
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  onLoadEarlier() {   // to load early msg from server, we need to fetch from api, show msg with limit
     this.setState((previousState) => {
       return {
-        messages: GiftedChat.append(previousState.messages, {
-          _id: Math.round(Math.random() * 1000000),
-          text: text.msg,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://facebook.github.io/react/img/logo_og.png',
-          },
-        }),
+        isLoadingEarlier: true,
       };
     });
-  }  
-  
-  onSend(messages = []) {
+
+    setTimeout(() => {
+      if (this._isMounted === true) {
+        this.setState((previousState) => {
+          return {
+            messages: GiftedChat.prepend(previousState.messages, require('./data/old_messages.js')),
+            loadEarlier: false,
+            isLoadingEarlier: false,
+          };
+        });
+      }
+    }, 1000); // simulating network
+  }
+
+  onSend(messages = []) {  // send user text msg to server , need to save with api
     this.socket.emit('client', {'id':1,"name":"ASO", 'msg':messages[0].text});
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+  }
+
+  onReceive(data) {  //receive txt msg from server , show typing... need to modify with new socket
+    var msg = data.text;
+    if (msg.length > 0) {
+        this.setState((previousState) => {
+          return {
+            typingText: 'Server  is typing...'
+          };
+        });
+    }
+    setTimeout(() => {
+      this.setState((previousState) => {
+        return {
+          messages: GiftedChat.append(previousState.messages,data),
+          typingText: null,
+          }
+      });
+    }, 1000);
+  } 
+
+  renderCustomActions(props) {
+    if (Platform.OS === 'ios') {
+      return (
+        <CustomActions
+          {...props}
+        />
+      );
+    }
+    const options = {
+      'Action 1': (props) => {
+        alert('option 1');
+      },
+      'Action 2': (props) => {
+        alert('option 2');
+      },
+      'Cancel': () => {},
+    };
+    return (
+      <Actions
+        {...props}
+        options={options}
+      />
+    );
+  }
+
+  renderBubble(props) {  // show bubble user name
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: '#f0f0f0',
+          }
+        }}
+      />
+    );
+  }
+
+  renderSystemMessage(props) {  // no need, just for show
+    return (
+      <SystemMessage
+        {...props}
+        containerStyle={{
+          marginBottom: 15,
+        }}
+        textStyle={{
+          fontSize: 14,
+        }}
+      />
+    );
+  }
+
+  renderCustomView(props) {  // for map
+    return (
+      <CustomView
+        {...props}
+      />
+    );
+  }
+
+  renderFooter(props) {  //for showing "Someone is typeing..."
+    if (this.state.typingText) {
+      return (
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>
+            {this.state.typingText}
+          </Text>
+        </View>
+      );
+    }
+    return null;
   }
 
   render() {
     return (
       <GiftedChat
         messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
+        onSend={this.onSend}
+        loadEarlier={this.state.loadEarlier}
+        onLoadEarlier={this.onLoadEarlier}
+        isLoadingEarlier={this.state.isLoadingEarlier}
+
         user={{
-          _id: 1,
+          _id: 1, // sent messages should have same user._id
         }}
+
+        renderActions={this.renderCustomActions}
+        renderBubble={this.renderBubble}
+        renderSystemMessage={this.renderSystemMessage}
+        renderCustomView={this.renderCustomView}
+        renderFooter={this.renderFooter}
       />
-    )
+    );
   }
 }
 
+const styles = StyleSheet.create({
+  footerContainer: {
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  footerText: {
+    fontSize: 14,
+    color: '#aaa',
+  },
+});
